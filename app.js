be245d298a66f67df31a64042fe80a183a2474eb
zaffaelma <zaffazidnielma@gmail.com>
2024-10-24 14:48:03 +0700

    selesai

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 function main() {
-  
-
-
-}
-
-module.exports = main
+    const hasilDadu = [1, 5, 2, 6, 2, 3]
+    kemunculanGanjil = 0
+    kemunculanGenap = 0
+    for(let i=0; i < hasilDadu.length; i++){
+      if (hasilDadu[i]%2!==0){
+          kemunculanGanjil++;
+      } else{
+          kemunculanGenap++;
+      }
+    }
+    console.log(`Raka mendapatkan angka ganjil sebanyak ${kemunculanGanjil} kali dan angka genap sebanyak ${kemunculanGenap} kali`)
+  }
+  main()
+  module.exports = main
